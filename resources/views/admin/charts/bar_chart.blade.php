14baeb124113d3aa7f28ba27a5f65113bbb3331e
Petra Yoshua Marturia <petratech1830@gmail.com>
2025-07-19 10:41:56 +0700

    Delete resources directory

--- resources/views/admin/charts/bar_chart.blade.php
+++
@@ -1,5 +0,0 @@
-{{-- resources/views/admin/charts/bar_chart.blade.php --}}
-{{-- Harapkan variabel $chartId --}}
-<div style="height: 300px; width: 100%;"> {{-- Sesuaikan ukuran --}}
-    <canvas id="{{ $chartId }}"></canvas>
-</div>
